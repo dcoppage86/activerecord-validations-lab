d3493e5190af2cb573daaaea2123961c41efb698
dcoppage86 <dakotakcoppage@me.com>
2021-03-10 00:14:38 -0700

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction),
+        message: "%{value} is not an option" }
+    
+    validate :validate_clickbait?
+
+    def validate_clickbait?
+        clickbait_phrases = [/won't believe/, /secret/, /guess/, /top [0-9]*/]
+        if !title.presence || clickbait_phrases.none?{ |phrase| phrase.match(title.downcase) }
+            errors.add(:title, "must be clickbait")
+        end
+    end
 end
